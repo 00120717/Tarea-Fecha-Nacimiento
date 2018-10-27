22d60155ba70d1680dde354bb110f6214284158d
Marvin Ramirez <00120717>
2018-10-27 06:22:39 +0200

    Tarea Fecha Nacimiento

--- script.js
+++ script.js
@@ -9,8 +9,9 @@ var person = {
 // Access to propertys
 console.log(person.name);
 // Change object's property value
-person.birthday = new Date(1994, 0, 11); // 11 - Jan - 1994
+person.birthday = new Date(1994, 1, 12); // 11 - Jan - 1994
 console.log(person.birthday);
+
 
 console.log(person.dui) // undefined
 person.dui = "000000000" // Assign 
@@ -52,12 +53,28 @@ console.log("Average age %i", list.reduce((sum, {
 
 
 // More readable
-
+/*
 function getAge(birthday) {
     var ageDifMs = Date.now() - birthday;
     var ageDate = new Date(ageDifMs); // miliseconds from epoch
-    console.log(ageDate + "fecha");
     return Math.abs(ageDate.getUTCFullYear() - 1970);
+}*/
+
+//Modificacion de la funcion anterior
+function getAge(birthday) {
+    var ageDifMs = Date.now() - birthday;
+    var ageDate = new Date(ageDifMs); // miliseconds from epoch
+    var age =  Math.abs(ageDate.getUTCFullYear() - 1970);
+
+    if(ageDate.getMonth == Date.now().getMonth){
+        if(ageDate.getDay == Date.now().getDay){
+            age -= 1;
+        }
+    }else if(ageDate.getDay == Date.now().getDay){
+        age -= 1;
+    }
+
+    return age;
 }
 
 console.log("Average age %i", list.reduce((sum, {
